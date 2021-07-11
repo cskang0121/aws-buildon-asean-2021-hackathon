30acad8ab96e81d0b6618d4a9fc4c1855ddd8fa4
Jenpoer <jeistory1@gmail.com>
2021-07-11 14:02:24 +0800

    First commit

---
+++ src/main/java/com/javawarriors/buyerside/BuyersideApplication.java
@@ -0,0 +1,13 @@
+package com.javawarriors.buyerside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyersideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javawarriors/buyerside/BuyersideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawarriors.buyerside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
