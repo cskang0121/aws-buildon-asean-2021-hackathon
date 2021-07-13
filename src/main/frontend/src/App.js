2ffdd570fd958a1a501ad61a4ed5552e6dcd0819
Michelle Leong <michelle@Michelles-MacBook-Air.local>
2021-07-13 18:04:39 +0800

    make offer

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -12,6 +12,8 @@ import CreateIFS from "./components/Pages/CreateIFS";
 import Login from "./components/Pages/Login";
 import { PrivateRoute } from "./components/PrivateRoute";
 import Search from "./components/Pages/Search";
+import WTBListingDetails from "./components/Pages/WTBListingDetails";
+import WTBOffer from "./components/Pages/WTBOffer";
 
 import { Container, Row, Col } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -29,6 +31,9 @@ function App() {
           <PrivateRoute path="/ifs" exact component={IFS} />
           <PrivateRoute path="/add-ifs" exact component={CreateIFS} />
           <PrivateRoute path="/search" exact component={Search} />
+          <PrivateRoute path="/wtb-listing-details" exact component={WTBListingDetails} />
+          <PrivateRoute path="/wtb-listing-details" exact component={WTBListingDetails} />
+          <PrivateRoute path="/offer-for-wtb" exact component={WTBOffer} />
         </Switch>
       </Router>
     </div>
